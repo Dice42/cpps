3cede91b4697c625d1513acaf32d956d0cae898f
Dice42 <mohammoh@student.42abudhabi.ae>
2024-09-10 16:16:30 +0400

    completed c05/ex01

---
+++ C05/ex02/Bureaucrat.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mohammoh <mohammoh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 09:17:44 by mohammoh          #+#    #+#             */
+/*   Updated: 2024/09/09 22:19:00 by mohammoh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <stdexcept>
+
+class Bureaucrat
+{
+	private:
+		const std::string	_name;
+		int					_grade;
+	
+	protected:
+		class GradeTooHighException : public std::exception {
+			public:
+				const char* what() const throw();
+		};
+		class GradeTooLowException	: public std::exception {
+			public:
+				const char* what() const throw();
+		};
+
+	public:
+		Bureaucrat(void);
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator=(const Bureaucrat& rhs);
+		~Bureaucrat(void);
+	
+		void				decrementGrade();
+		void				incrementGrade();
+		int					getGrade(void);
+		const std::string	getName(void);
+		
+};
+#endif
+
+/* overload insertion operator */
+std::ostream&	operator<<(std::ostream& os, Bureaucrat& my);
